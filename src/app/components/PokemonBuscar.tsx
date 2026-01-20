8690fe18a2694106a281bacd6e2e201982eeeee0
Darien Cuevas <gunfire2101@gmail.com>
2026-01-20 02:04:32 -0600

    fix location

--- src/app/components/PokemonBuscar.tsx
+++ src/app/components/PokemonBuscar.tsx
@@ -5,70 +5,72 @@ import { useState } from "react";
 type pokemon = {
   id: number;
   name: string;
-  location_area_encounters: location[];
+  location_area_encounters: string;
   sprites: Sprites;
   abilities: Ability[];
   types: Types[];
   stats: Stats[];
-  encounter_method: method[];
 };
 
-type location = {
-      location_area: {
-        name: string;
-        url: string;
-    }
-}
-
 type Sprites = {
-    front_default: string;
-    front_shiny: string;
-}
+  front_default: string;
+  front_shiny: string;
+};
 
 type Ability = {
-    is_hidden: boolean;
-    slot: number;
-        ability: {
-            name: string;
-            url: string;
-        }
-}
+  is_hidden: boolean;
+  slot: number;
+  ability: {
+    name: string;
+    url: string;
+  };
+};
 
 type Types = {
-    slot: number;
-        type: {
-            name: string;
-            url: string;
-    }
-}
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+};
 
 type Stats = {
-    base_stat: number;
-        stat: {
-            name: string;
-            url: string;
-        }
-}
+  base_stat: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+};
 
-type method = {
-  name: string;
-  order: string;
-    encounter_method: {
+type Location = {
+  location_area: {
+    name: string;
+  };
+  version_details: {
+    version: {
       name: string;
       url: string;
-    }
-}
+    };
+    encounter_details: {
+      method: {
+        name: string;
+      };
+      condition_values: {
+        name: string;
+      }[];
+    }[];
+  }[];
+};
 
 export default function PokemonBuscar() {
   const [name, setName] = useState("");
   const [pokemon, setPokemon] = useState<pokemon | null>(null);
   const [error, setError] = useState("");
-  const [locations, setLocations] = useState<location[]>([]);
+  const [locations, setLocations] = useState<Location[]>([]);
   const [sprites, setSprites] = useState<Sprites | null>(null);
   const [abilities, setAbilities] = useState<Ability[]>([]);
   const [types, setTypes] = useState<Types[]>([]);
   const [stats, setStats] = useState<Stats[]>([]);
-  const [method, setMethod] = useState<method[]>([])
 
   async function handleClick(element: React.FormEvent<HTMLFormElement>) {
     element.preventDefault();
@@ -93,33 +95,30 @@ export default function PokemonBuscar() {
       //locations
       const resLocations = await fetch(data.location_area_encounters);
       const localizacion = await resLocations.json();
+      console.log("location:", locations);
       setLocations(localizacion);
       console.log(localizacion);
 
       //sprites
+      console.log("sprites:", sprites);
       setSprites(data.sprites);
       console.log(data.sprites);
 
       //abilities
+      console.log("abilities:", abilities);
       setAbilities(data.abilities);
       console.log(data.abilities);
 
-        //types
+      //types
+      console.log("types:", types);
       setTypes(data.types);
       console.log(data.types);
 
-        //stats
-        setStats(data.stats);
-        console.log(data.stats);
-
-        //method
-        setMethod(data.method)
-        console.log(data.method)
-    
-    }
-
-
-    catch (error) {
+      //stats
+      console.log("stats:", stats);
+      setStats(data.stats);
+      console.log(data.types);
+    } catch (error) {
       setError("no se encontró el pokemon");
     }
   }
@@ -143,23 +142,36 @@ export default function PokemonBuscar() {
           <h2>{pokemon.name}</h2>
           <p>id: {pokemon.id}</p>
 
-          {method.length > 0 && (
-            <div>
-              <h3>metodo de encuentro:</h3>
-              <ul>
-                {method.map((method, index) => (
-                  <li key={index}>{method.encounter_method.name}</li>
-                ))}
-              </ul>
-            </div>
-          )}
+{locations.length > 0 && (
+  <div>
+    <h3>Método de encuentro:</h3>
+    <ul>
+      {locations.map((location, index) => (
+        <li key={index}>
+          Area: {location.location_area.name}
 
-                {locations.map((location, index) => (
-                  <div key={index}>
-                    <p>{location.location_area.name}</p>
-                    <p>{location.}</p>
-                  </div>
-                ))}
+          {location.version_details.map((version, vIndex) => (
+            <div key={vIndex}>
+              <p>Version: {version.version.name}</p>
+
+              {version.encounter_details.map((encounter, eIndex) => (
+                <div key={eIndex}>
+                  <p>Método: {encounter.method.name}</p>
+
+                  {encounter.condition_values.map((condition, cIndex) => (
+                    <p key={cIndex}>
+                      Condición: {condition.name}
+                    </p>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ))}
+        </li>
+      ))}
+    </ul>
+  </div>
+)}
 
 
           {sprites && (
